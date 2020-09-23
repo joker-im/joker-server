5127a490b51225b5433dc0ece05696d7937e11e0
cenmaokuan <cenmaokuan@jianke.com>
2020-09-23 19:22:19 +0800

    init

---
+++ src/test/java/org/im/joker/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.im.joker.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
